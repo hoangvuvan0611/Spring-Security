8d368eadaa1e69265f8eb8f8e1aba92a98f07528
Vũ Văn Hoàng <6656485@sv.vnua.edu.vn>
2024-06-17 10:11:18 +0700

    init commit

---
+++ src/main/java/org/vvh/springsecurity/entity/User.java
@@ -0,0 +1,24 @@
+package org.vvh.springsecurity.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+
+}
